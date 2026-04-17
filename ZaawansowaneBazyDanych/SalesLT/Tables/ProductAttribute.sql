3ec15e9a96ba2c98a543802bb11fb01d6755a2ce
m-adamek <236731@student.uek.krakow.pl>
2026-04-17 23:58:58 +0200

    update

---
+++ ZaawansowaneBazyDanych/SalesLT/Tables/ProductAttribute.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [SalesLT].[ProductAttribute] (
+    [ProductID] INT                                          NOT NULL,
+    [Info]      XML(CONTENT [dbo].[ProductAttributesSchema]) NULL,
+    PRIMARY KEY CLUSTERED ([ProductID] ASC),
+    CONSTRAINT [fk_ProductAttribute_Product] FOREIGN KEY ([ProductID]) REFERENCES [SalesLT].[Product] ([ProductID])
+);
+
---
+++ ZaawansowaneBazyDanych/dbo/XMLSchemaCollections/ProductAttributesSchema.sql
@@ -0,0 +1,18 @@
+﻿CREATE XML SCHEMA COLLECTION [dbo].[ProductAttributesSchema]
+    AS N'<xsd:schema xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+  <xsd:element name="Attributes" type="AttributesType" />
+  <xsd:complexType name="AttributesType">
+    <xsd:complexContent>
+      <xsd:restriction base="xsd:anyType">
+        <xsd:sequence>
+          <xsd:element name="Name" type="xsd:string" />
+          <xsd:element name="Price" type="xsd:decimal" />
+          <xsd:element name="Weight" type="xsd:decimal" />
+          <xsd:element name="Brand" type="xsd:string" />
+          <xsd:element name="Length" type="xsd:decimal" />
+        </xsd:sequence>
+      </xsd:restriction>
+    </xsd:complexContent>
+  </xsd:complexType>
+</xsd:schema>';
+
